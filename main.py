a3c7357afbf580520c3ee4fe399ffe3b3c60740a
mrjacob8282 <117129463+mrjacob8282@users.noreply.github.com>
2022-10-31 22:30:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+    input1 = input('first number to add:') #first number to add
+    input2 = input('second number to add:') #second number to add
+    added_numbers = input2 + input1 #math equation to add numbers
+    print('numbers added together equals', added_numbers) #numbers added together presented to user
