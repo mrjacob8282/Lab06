0cd2b243fd6af804466bea54461d82e909224cfb
McDaniel <mcdaniel@McDaniels-iMac.local>
2022-10-31 22:50:36 -0400

    updated lab project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-    input1 = input('first number to add:') #first number to add
-    input2 = input('second number to add:') #second number to add
-    added_numbers = input2 + input1 #math equation to add numbers
-    print('numbers added together equals', added_numbers) #numbers added together presented to user
+input1 = input('first number to add and subtract:') #first number to add
+input2 = input('second number to add and subtract:') #second number to add
+added_numbers = (int(input2) + int(input1)) #math equation to add numbers
+print('numbers added together equals', added_numbers) #numbers added together presented to user
+subtract_numbers = (int(input1) - int(input2)) #added subtraction equation to the problem
+print('numbers subtracted together equals', subtract_numbers) #numbers subtracted together presented to user added in
